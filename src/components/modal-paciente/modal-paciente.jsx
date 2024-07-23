fddd76db908b9a24e1f3a92e1f2863fff054ef42
unversedsix0 <gaba.olipires@gmail.com>
2024-07-23 20:59:23 -0300

    salvando dados do formulario

--- src/components/modal-paciente/modal-paciente.jsx
+++ src/components/modal-paciente/modal-paciente.jsx
@@ -1,19 +1,19 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable import/no-extraneous-dependencies */
-import React from 'react'
-
-import FormControl from '@mui/material/FormControl'; // Importação do FormControl
-import InputLabel from '@mui/material/InputLabel'; // Importação do InputLabel
-import Select from '@mui/material/Select'; // Importação do Select
-import MenuItem from '@mui/material/MenuItem'; // Importação do MenuItem
-import InputMask from 'react-input-mask'; // Importação do InputMask
+import React from 'react';
+import InputMask from 'react-input-mask';
+import { useForm, Controller } from 'react-hook-form';
 
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 import Modal from '@mui/material/Modal';
+import Select from '@mui/material/Select';
 import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
 import TextField from '@mui/material/TextField';
+import InputLabel from '@mui/material/InputLabel';
 import Typography from '@mui/material/Typography';
+import FormControl from '@mui/material/FormControl';
 
 const style = {
   position: 'absolute',
@@ -25,156 +25,274 @@ const style = {
   boxShadow: 2,
   p: 4,
   borderRadius: 2,
-  '@media (max-width:600px)': { 
-    width: '95%', 
+  '@media (max-width:600px)': {
+    width: '95%',
   },
 };
 
-
 export default function ModalPaciente(props) {
-  const {open, handleClose} = props;
+  const { open, handleClose } = props;
+  const { control, handleSubmit, formState: { errors } } = useForm();
+
+  const onSubmit = (data) => {
+    console.log('Dados do formulário:', data);
+
+    
+  };
+
   return (
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Adicionar Novo Paciente
-          </Typography>
-          <Box component="form" sx={{ mt: 2 }}>
-            <TextField
-              fullWidth
-              margin="normal"
-              label="Nome Completo"
-              variant="outlined"
-              required
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+        <Typography id="modal-modal-title" variant="h6" component="h2">
+          Adicionar Novo Paciente
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit(onSubmit)} sx={{ mt: 2 }}>
+          <Controller
+            name="nome"
+            control={control}
+            defaultValue=""
+            rules={{ required: 'Nome completo é obrigatório' }}
+            render={({ field }) => (
+              <TextField
+                {...field}
+                fullWidth
+                margin="normal"
+                label="Nome Completo"
+                variant="outlined"
+                error={!!errors.nome}
+                helperText={errors.nome ? errors.nome.message : ''}
+                required
+              />
+            )}
+          />
+          <Stack direction="row" spacing={2} sx={{ mt: 2 }}>
+            <Controller
+              name="cpf"
+              control={control}
+              defaultValue=""
+              rules={{ required: 'CPF é obrigatório' }}
+              render={({ field }) => (
+                <FormControl fullWidth margin="normal" variant="outlined">
+                  <InputLabel htmlFor="cpf">CPF</InputLabel>
+                  <InputMask
+                    mask="999.999.999-99"
+                    {...field}
+                  >
+                    {(inputProps) => (
+                      <TextField
+                        {...inputProps}
+                        id="cpf"
+                        variant="outlined"
+                        label="CPF"
+                        required
+                        InputLabelProps={{ shrink: true }}
+                        error={!!errors.cpf}
+                        helperText={errors.cpf ? errors.cpf.message : ''}
+                      />
+                    )}
+                  </InputMask>
+                </FormControl>
+              )}
             />
-            <Stack direction="row" spacing={2} sx={{ mt: 2 }}>
-              <FormControl fullWidth margin="normal" variant="outlined">
-                <InputLabel htmlFor="cpf" />
-                <InputMask mask="999.999.999-99" maskPlaceholder={null}>
-                  {(inputProps) => (
-                    <TextField
-                      {...inputProps}
-                      id="cpf"
-                      variant="outlined"
-                      label="CPF"
-                      placeholder="CPF"
-                      required
-                      InputLabelProps={{ shrink: true }} // Isso garante que o rótulo se mova para cima quando o campo está em uso
-                    />
-                  )}
-                </InputMask>
-              </FormControl>
-              <TextField
-                fullWidth
-                margin="normal"
-                label="Data de Nascimento"
-                type="date"
-                InputLabelProps={{ shrink: true }}
-                variant="outlined"
-                required
-              />
-            </Stack>
-            <Stack direction="row" spacing={2} sx={{ mt: 2 }}>
-              <FormControl fullWidth margin="normal" variant="outlined" required>
-                <InputLabel>Sexo</InputLabel>
-                <Select
-                  label="Sexo"
-                  defaultValue=""
+            <Controller
+              name="dataNascimento"
+              control={control}
+              defaultValue=""
+              rules={{ required: 'Data de nascimento é obrigatória' }}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  fullWidth
+                  margin="normal"
+                  label="Data de Nascimento"
+                  type="date"
+                  InputLabelProps={{ shrink: true }}
+                  variant="outlined"
                   required
-                  displayEmpty
-                >
-                  <MenuItem value="male">Masculino</MenuItem>
-                  <MenuItem value="female">Feminino</MenuItem>
-                  <MenuItem value="other">Outro</MenuItem>
-                </Select>
-              </FormControl>
-              <FormControl fullWidth margin="normal" variant="outlined" required>
-                <InputLabel>Estado Civil</InputLabel>
-                <Select
-                  label="Estado Civil"
-                  defaultValue=""
-                  required
-                  displayEmpty
-                >
-                  <MenuItem value="solteiro">Solteiro</MenuItem>
-                  <MenuItem value="casado">Casado</MenuItem>
-                  <MenuItem value="divorciado">Divorciado</MenuItem>
-                  <MenuItem value="viuvo">Viúvo</MenuItem>
-                </Select>
-              </FormControl>
-            </Stack>
-            <TextField
-              fullWidth
-              margin="normal"
-              label="E-mail"
-              variant="outlined"
-              required
-              sx={{ mt: 2 }}
+                  error={!!errors.dataNascimento}
+                  helperText={errors.dataNascimento ? errors.dataNascimento.message : ''}
+                />
+              )}
             />
-            <Stack direction="row" spacing={2} sx={{ mt: 2 }}>
-              <FormControl fullWidth margin="normal" variant="outlined">
-                <InputLabel htmlFor="cep" />
-                <InputMask mask="99999-999" maskPlaceholder={null}>
-                  {(inputProps) => (
-                    <TextField
-                      {...inputProps}
-                      id="cep"
-                      variant="outlined"
-                      label="CEP"
-                      placeholder="CEP"
-                      required
-                      InputLabelProps={{ shrink: true }}
-                    />
-                  )}
-                </InputMask>
-              </FormControl>
+          </Stack>
+          <Stack direction="row" spacing={2} sx={{ mt: 2 }}>
+            <Controller
+              name="sexo"
+              control={control}
+              defaultValue=""
+              rules={{ required: 'Sexo é obrigatório' }}
+              render={({ field }) => (
+                <FormControl fullWidth margin="normal" variant="outlined" required>
+                  <InputLabel>Sexo</InputLabel>
+                  <Select
+                    {...field}
+                    label="Sexo"
+                    displayEmpty
+                    error={!!errors.sexo}
+                  >
+                    <MenuItem value=""><em>Selecione</em></MenuItem>
+                    <MenuItem value="male">Masculino</MenuItem>
+                    <MenuItem value="female">Feminino</MenuItem>
+                    <MenuItem value="other">Outro</MenuItem>
+                  </Select>
+                  {errors.sexo && <Typography color="error">{errors.sexo.message}</Typography>}
+                </FormControl>
+              )}
+            />
+            <Controller
+              name="estadoCivil"
+              control={control}
+              defaultValue=""
+              rules={{ required: 'Estado civil é obrigatório' }}
+              render={({ field }) => (
+                <FormControl fullWidth margin="normal" variant="outlined" required>
+                  <InputLabel>Estado Civil</InputLabel>
+                  <Select
+                    {...field}
+                    label="Estado Civil"
+                    displayEmpty
+                    error={!!errors.estadoCivil}
+                  >
+                    <MenuItem value=""><em>Selecione</em></MenuItem>
+                    <MenuItem value="solteiro">Solteiro</MenuItem>
+                    <MenuItem value="casado">Casado</MenuItem>
+                    <MenuItem value="divorciado">Divorciado</MenuItem>
+                    <MenuItem value="viuvo">Viúvo</MenuItem>
+                  </Select>
+                  {errors.estadoCivil && <Typography color="error">{errors.estadoCivil.message}</Typography>}
+                </FormControl>
+              )}
+            />
+          </Stack>
+          <Controller
+            name="email"
+            control={control}
+            defaultValue=""
+            rules={{
+              required: 'E-mail é obrigatório',
+              pattern: {
+                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                message: 'E-mail inválido'
+              }
+            }}
+            render={({ field }) => (
               <TextField
+                {...field}
                 fullWidth
                 margin="normal"
-                label="Endereço"
+                label="E-mail"
                 variant="outlined"
                 required
+                error={!!errors.email}
+                helperText={errors.email ? errors.email.message : ''}
+                sx={{ mt: 2 }}
               />
-            </Stack>
-            {/* Campos Bairro e Número abaixo de CEP e Endereço */}
-            <Stack direction="row" spacing={2} sx={{ mt: 2 }}>
-              <TextField
-                fullWidth
-                margin="normal"
-                label="Bairro"
-                variant="outlined"
-                required
-              />
-              <TextField
-                fullWidth
-                margin="normal"
-                label="Número"
-                variant="outlined"
-                required
-                type="number" // Garante que somente números podem ser inseridos
-              />
-            </Stack>
-            {/* Botão Salvar */}
-            <Box sx={{ display: 'flex', justifyContent: 'flex-end', mt: 2 }}>
-              <Button
-                variant="contained"
-                color="primary"
-                sx={{ backgroundColor: '#B0E0E6', color: '#000', '&:hover': { backgroundColor: '#ADD8E6' } }}
-                onClick={() => {
-                    handleClose()
-                
-                }}
-              >
-                Salvar
-              </Button>
-            </Box>
+            )}
+          />
+          <Stack direction="row" spacing={2} sx={{ mt: 2 }}>
+            <Controller
+              name="cep"
+              control={control}
+              defaultValue=""
+              rules={{ required: 'CEP é obrigatório' }}
+              render={({ field }) => (
+                <FormControl fullWidth margin="normal" variant="outlined">
+                  <InputLabel htmlFor="cep">CEP</InputLabel>
+                  <InputMask
+                    mask="99999-999"
+                    {...field}
+                  >
+                    {(inputProps) => (
+                      <TextField
+                        {...inputProps}
+                        id="cep"
+                        variant="outlined"
+                        label="CEP"
+                        required
+                        InputLabelProps={{ shrink: true }}
+                        error={!!errors.cep}
+                        helperText={errors.cep ? errors.cep.message : ''}
+                      />
+                    )}
+                  </InputMask>
+                </FormControl>
+              )}
+            />
+            <Controller
+              name="endereco"
+              control={control}
+              defaultValue=""
+              rules={{ required: 'Endereço é obrigatório' }}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  fullWidth
+                  margin="normal"
+                  label="Endereço"
+                  variant="outlined"
+                  required
+                  error={!!errors.endereco}
+                  helperText={errors.endereco ? errors.endereco.message : ''}
+                />
+              )}
+            />
+          </Stack>
+          <Stack direction="row" spacing={2} sx={{ mt: 2 }}>
+            <Controller
+              name="bairro"
+              control={control}
+              defaultValue=""
+              rules={{ required: 'Bairro é obrigatório' }}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  fullWidth
+                  margin="normal"
+                  label="Bairro"
+                  variant="outlined"
+                  required
+                  error={!!errors.bairro}
+                  helperText={errors.bairro ? errors.bairro.message : ''}
+                />
+              )}
+            />
+            <Controller
+              name="numero"
+              control={control}
+              defaultValue=""
+              rules={{ required: 'Número é obrigatório' }}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  fullWidth
+                  margin="normal"
+                  label="Número"
+                  variant="outlined"
+                  required
+                  type="number"
+                  error={!!errors.numero}
+                  helperText={errors.numero ? errors.numero.message : ''}
+                />
+              )}
+            />
+          </Stack>
+          <Box sx={{ display: 'flex', justifyContent: 'flex-end', mt: 2 }}>
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              sx={{ backgroundColor: '#B0E0E6', color: '#000', '&:hover': { backgroundColor: '#ADD8E6' } }}
+            >
+              Salvar
+            </Button>
           </Box>
         </Box>
-      </Modal>
-  )
+      </Box>
+    </Modal>
+  );
 }
